80634818da8ffa42d56e2a2f6a6420bd803266bf
Steven DeLitta <sdelitta@gmail.com>
2022-03-07 18:43:17 -0500

    finished styling

---
+++ app.js
@@ -0,0 +1,8 @@
+let quest = document.querySelector("#questions")
+
+let ansOne = document.querySelector("#ans1")
+let ansTwo = document.querySelector("#ans2")
+let ansThree = document.querySelector("#ans3")
+
+
+
