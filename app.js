72622f39586b92bfc0938bc4f2c938f496d8458f
Steven DeLitta <sdelitta@gmail.com>
2022-03-09 09:58:28 -0500

    refactoring conditionalsto for loops

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 let questions =
-    {
-        question1: "Who portrayed the nightmare demon named Freddy Krueger?",
-        question2: "What is the name of the chainsaw weilding cannibal from Texas?",
-        question3: "Who directed Night Of The Living Dead?"
-    }
-
-let quest1 = questions.question1
-let quest2 = questions.question2
-let quest3 = questions.question3
+    [
+        "Who portrayed the nightmare demon named Freddy Krueger?",
+        "What is the name of the chainsaw weilding cannibal from Texas?",
+        "Who directed Night Of The Living Dead?"
+    ]
 
 const markWrong = document.getElementById("score-wrong")
 const markCorrect = document.getElementById("score-correct")
@@ -18,51 +14,133 @@ let ansOne = document.querySelector("#ans1")
 let ansTwo = document.querySelector("#ans2")
 let ansThree = document.querySelector("#ans3")
 
+let answers = [
+    [answer1, answer2, answer3],
+    [answer4, answer5, answer6],
+    [answer7, answer8, answer9]
+]
 
-question.innerText = quest1
 
+question.innerText = questions[0]
 const questFunction = () => {
-    question.innerText = quest1
-        if (markCorrect.innerText === "*") {
-            question.innerText = quest2
-    }
-        if (markWrong.innerText === "/"){
-            question.innerText = quest2
+    for (let i=0; i<questions.length; i++){
+        // if(questions[0]) {
+        //     question.innerText = questions[0]
+        //     ansOne.innerText = "Gunnar Hansen"
+        //     ansTwo.innerText = "Robert Englund"
+        //     ansThree.innerText = "Jason Voorhees"
+
+        // }
+        // if(markCorrect.innerText === "*") {
+        //     question.innerText = questions[1]
+        // }
+        question = questions[i]
+        for (let n = 0; n<answers.length; n++){
+            ansOne = answers[i][n]
+        }
     }
 }
+
+////////////////////////////////////////////////////////
+// question.innerText = questions[0]
+// ansOne.innerText = "Gunnar Hansen"
+// ansTwo.innerText = "Robert Englund"
+// ansThree.innerText = "Jason Voorhees"
+    
+// const questFunction = () => {
+//         if (markCorrect.innerText === "*") {
+//             question.innerText = questions[1]
+//             ansOne.innerText = "Leatherface"
+//             ansTwo.innerText = "Jigsaw"
+//             ansThree.innerText = "Pinhead"
+//             // questFunction()
+//     } else 
+//         if (markWrong.innerText === "/"){
+//             ansOne.innerText = "Leatherface"
+//             ansTwo.innerText = "Jigsaw"
+//             ansThree.innerText = "Pinhead"
+//             question.innerText = questions[1]
+//             // questFunction()
+//     }
+//         if (markCorrect.innerText === "**") {
+//             question.innerText = questions[2]
+//             ansOne.innerText = "John Carpenter"
+//             ansTwo.innerText = "George Romero"
+//             ansThree.innerText = "Wes Craven"
+//             // questFunction()
+//     }
+//         if (markWrong.innerText === "//"){
+//             ansOne.innerText = "John Carpenter"
+//             ansTwo.innerText = "George Romero"
+//             ansThree.innerText = "Wes Craven"
+//             question.innerText = questions[2]
+//             // questFunction()
+//     }
+// }
+
 
 questFunction()
 
 
 
-
-
 ansOne.addEventListener("click", () => {
-    if(question = questions.question1){
-        ansOne = markWrong.innerText = "/"
-    } else if (question = questions.question2) {
-        ansOne = markCorrect.innerText = "* *"
-    } else if (question = questions.question3){
-        ansOne = markWrong.innerText = "/ / /"
+    if(question === questions[0]){
+        markWrong.innerText = "/"
+    } else if (question === questions[1]) {
+        markCorrect.innerText = "* *"
+    } else if (question === questions[2]){
+        markWrong.innerText = "/ / /"
     }
+    questFunction()
 })
 
 ansTwo.addEventListener("click", () => {
-    if(question = questions.question1){
-        ansOne = markCorrect.innerText = "*"
-    } else if (question = questions.question2) {
-        ansOne = markWrong.innerText = "/ /"
-    } else if (question = questions.question3){
-        ansOne = markCorrect.innerText = "* * *"
+    if(question = questions[0]){
+        markCorrect.innerText = "*"
+    } else if (question = questions[1]) {
+        markWrong.innerText = "/ /"
+    } else if (question = questions[2]){
+        markCorrect.innerText = "* * *"
     }
+    questFunction()
 })
 
 ansThree.addEventListener("click", () => {
-    if(question = questions.question1){
-        ansOne = markWrong.innerText = "/"
-    } else if (question = questions.question2) {
-        ansOne = markWrong.innerText = "/ /"
-    } else if (question = questions.question3){
-        ansOne = markWrong.innerText = "/ / /"
+    if(question = questions[0]){
+        markWrong.innerText = "/"
+    } else if (question = questions[1]) {
+        markWrong.innerText = "/ /"
+    } else if (question = questions[2]){
+        markWrong.innerText = "/ / /"
     }
+    questFunction()
 })
+
+
+
+
+
+
+
+// const question1 = () => {
+//     question.innerText = questions[0]
+//     ansOne.innerText = "Gunnar Hansen"
+//     ansTwo.innerText = "Robert Englund"
+//     ansThree.innerText = "Jason Voorhees"
+// }
+
+// question1()
+
+
+
+// ansOne.addEventListener("click", () => {
+//     if(question = question[0]){
+        
+//     }
+// })
+// ansTwo.addEventListener("click", () => {
+
+// })
+// ansThree.addEventListener("click", () => {
+
+// })
